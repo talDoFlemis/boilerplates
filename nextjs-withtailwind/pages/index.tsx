5c7e67a52dabe05dd48fd31672a4cafa3f95c493
Flemis <coderflemis@gmail.com>
2022-03-10 19:14:09 -0300

    removing some unused project files

--- nextjs-withtailwind/pages/index.tsx
+++ nextjs-withtailwind/pages/index.tsx
@@ -1,20 +1,7 @@
-import Navbar from "@components/home/Navbar"
-import Sidebar from "@components/home/Sidebar"
-import Head from "next/head"
-
 const Home = () => {
-  return (
-    <div className="flex min-h-screen w-screen bg-gradient-to-b from-neutral-focus via-neutral to-spotifyBlack">
-      <Head>
-        <title>Spotify</title>
-      </Head>
-      <Sidebar />
-      <div className="w-full">
-        <Navbar />
-        <main className="p-8 text-white">Hellow</main>
-      </div>
-    </div>
-  )
+  return 
+    <div>Hellow</div>
+  
 }
 
 export default Home
